86686757e8debfa7adb38b049eb3c2b319b0b873
ibrahiemhss <kekoreko3@gmail.com>
2017-05-29 01:08:23 +0300

    new changes in commentssssssssssssssssssssss

--- app/src/main/java/com/example/administrator/complettedmyspli/Comments/DialogeComments.java
+++ app/src/main/java/com/example/administrator/complettedmyspli/Comments/DialogeComments.java
@@ -7,18 +7,25 @@ import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.administrator.complettedmyspli.Mysingletone;
 import com.example.administrator.complettedmyspli.R;
 
 import org.json.JSONArray;
@@ -26,7 +33,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Administrator on 24/05/2017.
@@ -39,7 +48,7 @@ public class DialogeComments extends Dialog {
     RecyclerView.Adapter  recyclerViewadapterComment;
     ProgressBar progressBar;
     String URLcommest = "http://devsinai.com/DrSiani/GetIdComments.php";
-    String URLpost="http://devsinai.com/DrSiani/getOnePost.php";
+    String URLAddComment="http://devsinai.com/DrSiani/imageUploadPostDr/AddComment.php";
     String JSON_ID = "user_id";
     String JSON_NAME = "name";
     String JIMAG_Name = "imagepost";
@@ -65,8 +74,8 @@ public class DialogeComments extends Dialog {
 
     public Dialog d;
     public Button yesSignoutt,noySignout;
-    TextView textComent;
-    EditText editcomment;
+    TextView txtAddComment;
+    EditText editTextComments;
 
 
     public DialogeComments(Context a) {
@@ -86,6 +95,9 @@ public class DialogeComments extends Dialog {
         params.height = WindowManager.LayoutParams.WRAP_CONTENT;
         getWindow().setAttributes((WindowManager.LayoutParams) params);
 
+        editTextComments= (EditText) findViewById(R.id.editTextComments);
+        txtAddComment= (TextView) findViewById(R.id.txtAddComment);
+
         recyclerView=(RecyclerView)findViewById(R.id.RvListComent) ;
         Mylist = new ArrayList<>();
         JSON_DATA_WEB_CALL();
@@ -103,7 +115,64 @@ public class DialogeComments extends Dialog {
         id_Comment = prefComment.getString("post_id", "post_id");
         editorComment = prefComment.edit();
 
+        txtAddComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String Comment=editTextComments.getText().toString();
 
+
+                if(Comment.equals("")){
+                    Toast.makeText(DialogeComments.this.c,"ادخل تعليق",Toast.LENGTH_LONG).show();
+
+                }
+                else {
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, URLAddComment,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    try {
+                                        JSONArray jsonArray = new JSONArray(response);
+                                        JSONObject jsonObject = jsonArray.getJSONObject(0);
+
+                                        prefComment =c.getSharedPreferences("prefCommentId.conf", Context.MODE_PRIVATE);
+                                        id_Comment = prefComment.getString("post_id", "post_id");
+                                        editorComment = prefComment.edit();
+
+                                        editorComment.putString("post_id", jsonObject.getString("post_id"));
+
+
+                                        editorComment.commit();
+
+                                        String code = jsonObject.getString("code");
+
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(DialogeComments.this.c, "Error", Toast.LENGTH_LONG).show();
+                            VolleyLog.e("Error: ", error.getMessage());
+                            error.printStackTrace();
+                        }}) {
+                        @Override
+                        protected Map<String, String> getParams() throws AuthFailureError {
+
+                            Map<String, String> params = new HashMap<String, String>();
+                            params.put("comment", Comment);
+                            params.put("post_id", id_Comment);
+
+
+
+                            return params;
+                        }
+                    };
+                    Mysingletone.getInstance(DialogeComments.this.c).addToRequestque(stringRequest);
+
+                }}
+        });
 
     }  public void JSON_DATA_WEB_CALL() {
 
